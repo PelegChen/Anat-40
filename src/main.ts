082baa1cb97d10b56e29ec9e89279b449d074c9b
Chen Peleg <chenpeleg@gmail.com>
2024-06-26 15:13:39 +0300

    loaded the daisy

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,13 @@
-import * as THREE from 'three';
+// @ts-ignore
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import {Object3D, Object3DEventMap, Scene} from 'three';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
-let camera: THREE.Camera, scene: THREE.Scene, renderer: THREE.WebGLRenderer;
 
-init();
-
-function init() {
-
-    const container = document.createElement( 'div' );
-    document.body.appendChild( container );
-
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-    camera.position.set( - 1.8, 0.6, 2.7 );
-
-    scene = new THREE.Scene();
-
-    new RGBELoader()
-        .setPath( 'textures/equirectangular/' )
-        .load( 'royal_esplanade_1k.hdr', function ( texture ) {
-
-            texture.mapping = THREE.EquirectangularReflectionMapping;
-
-            scene.background = texture;
-            scene.environment = texture;
-
-            render();
-
-            // model
-
-            const loader = new GLTFLoader().setPath( 'src/models/' );
-            loader.load( 'Daisy.glb', async function ( gltf ) {
-
-                const model = gltf.scene;
-
-                // wait until the model can be added to the scene without blocking due to shader compilation
-
-                await renderer.compileAsync( model, camera, scene );
-
-                scene.add( model );
-
-                render();
-
-            } );
-
-        } );
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = 1;
-    container.appendChild( renderer.domElement );
-
-    const controls = new OrbitControls( camera, renderer.domElement );
-    controls.addEventListener( 'change', render ); // use if there is no animation loop
-    controls.minDistance = 2;
-    controls.maxDistance = 10;
-    controls.target.set( 0, 0, - 0.2 );
-    controls.update();
-
-    window.addEventListener( 'resize', onWindowResize );
-
-}
-
-function onWindowResize() {
-
-    // @ts-ignore
-    camera.aspect = window.innerWidth / window.innerHeight;
-    // @ts-ignore
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    render();
-
-}
-
-//
-
-function render() {
-
-    renderer.render( scene, camera );
-
-}
+const loader = new GLTFLoader();
+export const scene = new Scene();
+loader.load('/Daisy.glb', (gltf: { scene: Object3D<Object3DEventMap>; }) => {
+    scene.add(gltf.scene);
+}, undefined, (error: any) => {
+    console.error('An error happened', error);
+});
