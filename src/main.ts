b4611fdf93b812738559eb6912a2dc2f2cdc734b
Chen Peleg <chenpeleg@gmail.com>
2024-06-26 15:49:06 +0300

    3js models not showing how they should

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 // @ts-ignore
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-import {  Object3D, Object3DEventMap} from 'three';
-
-
-import * as THREE from 'three'
+import * as THREE from 'three';
+import {AmbientLight, Object3D, Object3DEventMap} from 'three';
 // @ts-ignore
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
@@ -15,21 +13,21 @@ const sizes = {
     height: window.innerHeight
 };
 
-// const geometry = new THREE.SphereGeometry(3, 64, 64);
-// const material = new THREE.MeshStandardMaterial({color: '#7dfb8e'});
-// const mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
 
 const light = new THREE.PointLight(0xffffff, 200, 40);
 light.position.set(0, 10, 10);
 
-const lights = new THREE.AmbientLight(0xffffff, 200);
+const lights = new THREE.PointLight(0xffffff, 1);
+const ambientLight = new AmbientLight(0xFFFFFF);
+ambientLight.intensity = 50;
 scene.add(light);
 scene.add(lights);
+scene.add(ambientLight);
 
 //Camera
-const camera = new THREE.PerspectiveCamera(1 , sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(2, sizes.width / sizes.height);
 camera.position.z = 10
+camera.position.y = 0
 scene.add(camera);
 
 
@@ -49,11 +47,9 @@ renderer.render(scene, camera);
 
 
 const loader = new GLTFLoader();
-loader.load('/Daisy.glb', (gltf: { scene: Object3D<Object3DEventMap> }) => {
-   console.log(gltf)
-    // const mesh =
-    //     new Mesh(gltf.scene, gltf.materials)
-        scene.add(gltf.scene);
+loader.load('/Gingerbread House.glb', (gltf: { scene: Object3D<Object3DEventMap> }) => {
+    gltf.scene.position.y = -0.09;
+    scene.add(gltf.scene);
 }, undefined, (error: any) => {
     console.error('An error happened', error);
 });
