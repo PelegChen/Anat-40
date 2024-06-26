0a018709994d2e40fd4b8688041302150ab9f76f
Chen Peleg <chenpeleg@gmail.com>
2024-06-26 14:46:01 +0300

    adding animation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
 
 import * as THREE from 'three'
+// @ts-ignore
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
 
@@ -10,14 +12,13 @@ const sizes = {
     height: window.innerHeight
 };
 
-const geometry =
-    new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({ color: "#7dfb8e" });
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({color: '#7dfb8e'});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-const light = new THREE.PointLight(0xffffff, 20,40);
- light.position.set( 0,10,10);
+const light = new THREE.PointLight(0xffffff, 20, 40);
+light.position.set(0, 10, 10);
 scene.add(light);
 
 //Camera
@@ -26,22 +27,33 @@ camera.position.z = 10
 scene.add(camera);
 
 
-const canvas  = document.querySelector('.webgl') as HTMLCanvasElement
+const canvas = document.querySelector('.webgl') as HTMLCanvasElement
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Rederer
 const renderer = new THREE.WebGLRenderer({
     canvas
 });
+renderer.setPixelRatio(2)
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 
-
-window.addEventListener('resize', async() => {
+window.addEventListener('resize', async () => {
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
     camera.aspect = sizes.width / sizes.height;
     camera.updateProjectionMatrix();
     renderer.setSize(sizes.width, sizes.height);
-   renderer.render(scene, camera);
-
-
 })
+
+const loop = () => {
+    controls.update();
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(loop);
+
+}
+loop();
